0b833996f292d831d13fb3c5f6a03d35da5bec9c
Mateus00 <mateusoliveiranf@gmail.com>
2025-05-17 15:43:53 -0300

    Criando Sitemap para indexação no google searchs

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,25 +1,28 @@
-"""
-URL configuration for django_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import Sitemap
+
+from blog.models import Blog
 from . import views
+
+# Sitemap para os posts do blog
+class BlogSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        return Blog.objects.all()
+
+    def location(self, item):
+        return item.get_url_absoluta()
+
+sitemaps = {
+    'blog': BlogSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,10 +31,9 @@ urlpatterns = [
     path('sobre/', views.sobre),
     path('blog/', include('blog.urls')),
     path('loja/', include('loja.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('google39cdce1ef57976f4.html', TemplateView.as_view(template_name='google39cdce1ef57976f4.html', content_type='text/html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-    
